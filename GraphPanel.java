799134d36bb7958b21af58577a7cdc941eb9b6f5
Daniel Binoy <bleeblob43@gmail.com>
2018-04-07 11:18:34 -0700

    even more comments!

--- GraphPanel.java
+++ GraphPanel.java
@@ -2,38 +2,38 @@ import java.awt.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public  class GraphPanel extends JPanel{
+public  class GraphPanel extends JPanel{//a jpanel we put inside the jframe to contain the graph
 
 		private GraphHandler handler;
-		private double scaleX = 50;
+		private double scaleX = 50;//allows you to fit more points on screen 
 		private double scaleY = 10;
-		private int xOffset = 500;
-		private int yOffset = 500;
+		private int xOffset = 500;//controls where center of graph should be
+		private int yOffset = 500;//this would be halfway because 500 pix / 1000 pix
 		public GraphPanel(GraphHandler handler){
 			this.handler = handler;
 		}
 
 
 		@Override
-		public void paintComponent(Graphics g){
+		public void paintComponent(Graphics g){//how you actually draw the points and lines
+			//get the mouse location and store x and y to mouseX and mouseY
 			PointerInfo a = MouseInfo.getPointerInfo();
-      Point b = a.getLocation();
-      int mouseX = (int) (b.getX());
-      int mouseY = (int) (b.getY());
+      			Point b = a.getLocation();
+      			int mouseX = (int) (b.getX());
+      			int mouseY = (int) (b.getY());
 
-			xOffset = (int)(mouseX-getLocationOnScreen().getX());
+			xOffset = (int)(mouseX-getLocationOnScreen().getX());//change location of the origin based on position of mouse
 			yOffset = (int)(mouseY-getLocationOnScreen().getY());;
-      //set up graphics
-			Graphics2D g2d = (Graphics2D) g;
-      //clear
-			g2d.setColor(Color.BLACK);
-			g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
-			g2d.setColor(Color.RED);
-			for(Coordinate c: handler.getCoordinates()){
-				double y = yOffset-(scaleY*c.getY());
-				g2d.fillRect((int)(c.getX()*scaleX)+xOffset,(int)y,5,5);
+    
+			Graphics2D g2d = (Graphics2D) g;//you pretty much always do this if you want to do any graphics
+			g2d.setColor(Color.BLACK);//set the paint color to black
+			g2d.fillRect(0, 0, this.getWidth(), this.getHeight());//clear out the screen with black
+			g2d.setColor(Color.RED);//set paint color to red
+			for(Coordinate c: handler.getCoordinates()){//for each coordinate in the coordinate list from the handler
+				double y = yOffset-(scaleY*c.getY());//figure out where to piut it on screen based on scale, offset and actual coordinate
+				g2d.fillRect((int)(c.getX()*scaleX)+xOffset,(int)y,5,5);//draw a tiny rectangle for each point, w = 5, h = 5
 			}
-			g2d.drawLine(xOffset,this.getHeight(),xOffset,0);
+			g2d.drawLine(xOffset,this.getHeight(),xOffset,0);//draw the x and y axis
 			g2d.drawLine(0,yOffset,this.getWidth(),yOffset);
 
 		}
